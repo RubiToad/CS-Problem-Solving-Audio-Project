d8122b1d2d9cd77f9341c24862e717cdc8074814
RubiToad <150494954+RubiToad@users.noreply.github.com>
2023-11-26 15:59:08 -0500

    Check if wav and convert to wav
    Change added by Michael

--- audioload.py
+++ audioload.py
@@ -2,6 +2,9 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
 import pygame
+import os
+import shutil
+import soundfile as sf
 
 root = tk.Tk()
 root.title("Media Player")
@@ -9,25 +12,53 @@ root.geometry("300x200")
 
 pygame.init()
 
+
 def browse_file():
-    filepath = filedialog.askopenfilename(filetypes=[("mp3 files", "*.mp3"), ("Video Files", "*.mp4"), (".wav files", "*.wav"), (".ogg files", "*.ogg")])
-    play_file(filepath)
+    filepath = filedialog.askopenfilename(
+        filetypes=[("mp3 files", ".mp3"), ("Video Files", ".mp4"), (".wav files", ".wav"), (".ogg files", ".ogg")])
+    if filepath:
+        if not is_wav(filepath):
+            print("File is not WAV. Converting to WAV...")
+            filepath = wav_convert(filepath)
+            if is_wav(filepath):
+                print("File is now WAV")
+        else:
+            print("File is WAV")
+        play_file(filepath)
+
+
+def iswav(filepath):
+    , file_extension = os.path.splitext(filepath)
+    return file_extension.lower() == '.wav'
+
+
+def wav_convert(filepath):
+    wav_filepath = filepath.replace(os.path.splitext(filepath)[1], '.wav')
+    shutil.copyfile(filepath, wav_filepath)
+    return wav_filepath
+
 
 def play_file(filepath):
-    pygame.mixer.music.load(filepath)
-    pygame.mixer.music.play()
+    audio_data, sample_rate = sf.read(filepath, dtype='int16')
+    pygame.mixer.Sound(audio_data).play()
+
 
 def pause_file():
-    pygame.mixer.music.pause()
+    pygame.mixer.pause()
+
 
 def resume_file():
-    pygame.mixer.music.unpause()
+    pygame.mixer.unpause()
+
 
 def stop_file():
-    pygame.mixer.music.stop()
+    pygame.mixer.stop()
+
 
 def about_us():
-    messagebox.showinfo("About Media Player", "A simple media player program created in Python using Tkinter and Pygame.")
+    messagebox.showinfo("About Media Player",
+                        "A simple media player program created in Python using Tkinter and Pygame.")
+
 
 browse_button = tk.Button(root, text="Load Audio File", command=browse_file)
 browse_button.pack()
