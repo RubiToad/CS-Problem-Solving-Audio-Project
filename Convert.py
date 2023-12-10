d01bca35c9f8e5f3aac0e25241b533497e612990
Benjamin-Voor <benjamin.a.voor@outlook.com>
2023-12-09 20:49:44 -0500

    Fully modularized wav_convert function to Convert.py with compute_wav function. Unsuccessful trying to modularize display_time_waveform.

--- Convert.py
+++ Convert.py
@@ -7,8 +7,20 @@ def is_wav(filepath):
     file_extension = os.path.splitext(filepath)[1]
     return file_extension.lower() == '.wav'
 
+def compute_wav(filepath):
+    if not is_wav(filepath):
+        print("File is not WAV. Converting to WAV...")
+        wav_filepath = wav_convert(filepath)
+        if is_wav(filepath):
+            print("File is now WAV")
+
+    else:
+        print("File is WAV")
+        wav_filepath = filepath
+    return wav_filepath
+
 
 def wav_convert(filepath):
     wav_filepath = filepath.replace(os.path.splitext(filepath)[1], '.wav')
-    shutil.copyfile(filepath, wav_filepath)  # from=filepath, to=wav_filepath
-    return wav_filepath
+    shutil.copyfile(filepath, wav_filepath)
+
